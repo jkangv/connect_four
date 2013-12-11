a9ef5c2c2ab0a0429774130536d9e8a8ac94d9a0
Jihun Kang <jkangpz@gmail.com>
2013-12-11 14:23:22 -0500

    ai making process 4.3

--- lib/ai.rb
+++ lib/ai.rb
@@ -9,7 +9,7 @@ class AI
     @move = nil
   end
 
-  def winnable?
+  def win_move
     winnable = false
     column = 1
     until column == 8
@@ -20,12 +20,12 @@ class AI
       break if winnable
       column += 1
     end
-    winnable
+    @move
   end
 
-  def must_block?
-    must_block = false
+  def block_move
     column = 1
+    must_block = false
     @board.piece = @opponent.piece
     until column == 8
       if @board.win_move?(column)
@@ -36,34 +36,30 @@ class AI
       column += 1
     end
     @board.piece = self.piece
-    must_block
+    @move
   end
 
   def calculate_move
 
   end
 
-  def initial_moves?
-    initial_moves = false
+  def initial_move
     if @board.empty_board?
       @move = 4
-      initial_moves = true
-    elsif @board.board[5][4] == " #{@opponent.piece} " && @board.board[4].all? {|piece| piece == "   "} && @board.board[5].count(" #{opponent.piece} ") == 1
+    elsif @board.board[5][3] == " #{@opponent.piece} " && @board.board[4].all? {|piece| piece == "   "} && @board.board[5].count(" #{opponent.piece} ") == 1
+      @move = rand(3..5)
+    elsif @board.board[5][3] == "   " && @board.board[4].all? {|piece| piece == "   "} && @board.board[5].count(" #{opponent.piece} ") == 1
       @move = 4
-      initial_moves = true
-    elsif @board.board[5][4] == "   " && @board.board[4].all? {|piece| piece == "   "} && @board.board[5].count(" #{opponent.piece} ") == 1
-      @move = 4
-      initial_moves = true
     end
-    initial_moves
+    @move    
   end
 
   def make_move
-    if self.initial_moves?
+    if self.initial_move
       @board.move(@move)
-    elsif self.winnable?
+    elsif self.win_move
       @board.move(@move)
-    elsif self.must_block?
+    elsif self.block_move
       @board.move(@move)
     elsif self.calculate_move
       @board.move(@move)
